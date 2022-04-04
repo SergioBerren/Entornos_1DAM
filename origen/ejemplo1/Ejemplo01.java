e06e0208839be4fee6356229c60564861fa85d4e
SergioBerren <101136001+SergioBerren@users.noreply.github.com>
2022-04-04 17:57:38 +0200

    Hola que tal como estás?

--- origen/ejemplo1/Ejemplo01.java
+++ origen/ejemplo1/Ejemplo01.java
@@ -4,6 +4,5 @@ public class Ejemplo01 {
 	public static void main(String[]args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Que tal?");
-		System.out.println("Tercer Commit");
 	}
 }
